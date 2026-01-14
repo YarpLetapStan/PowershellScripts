d46620b2cd76e02e0a7bc28057ba160c8efbbd38
YarpLetapStan <salsa.isipog@gmail.com>
2026-01-13 17:28:47 -0800

    Update JarExecutionChecker.ps1

--- JarExecutionChecker.ps1
+++ JarExecutionChecker.ps1
@@ -3,7 +3,6 @@ Write-Host "Made by YarpLetapStan`nDM YarpLetapStan for Questions or Bugs`n" -Fo
 
 # ASCII Art Title - Simple symbols style
 $asciiTitle = @"
-
  __    __  _____  ______  _____  _       _____  _____  _____  _____  _____  _____  _____  __    _    ___   _____           
  \ \  / / /  _  \ | ___ \| ___ \| |     | ____||_   _|/  _  \| ___ \/  ___||_   _|/  _  \ |  \ | |  /  /  /  ___|       
   \ \/ /  | |_| | | |_/ /| |_/ /| |     | |__    | |  | |_| || |_/ /\ `--.   | |  | |_| | |   \| | /__/   \ `--.          
@@ -11,22 +10,24 @@ $asciiTitle = @"
     | |   | | | | | |\ \ | |    | |___  | |___   | |  | | | || |    /\__/ /  | |  | | | | | | \  |         /\__/ /      
     \_/   \_| |_/ \_| \_|\_|    |_____| |_____|  \_/  \_| |_/\_|    \____/   \_/  \_| |_/ \_| |__/         \____/        
                                                                                                          
-       __   __  _____  ____     _____ __   _  _____  _    __    __  _____ _____ ______             
-      |  \/  |/  _  \|  _ \   /  _  \|  \ | |/  _  \| |   \ \  / / |___  ||  ___|| ___ \            
-      | .  . || | | || | | |  | |_| ||   \| || |_| || |    \ \/ /     / / | |__  | |_/ /            
-      | |\/| || | | || | | |  |  _  || |\  ||  _  || |      \  /     / /  |  __| |    /             
-      | |  | || |_| || |/ /   | | | || | \ || | | || |____   | |    / /___| |___ | |\ \             
-      \_|  |_/\_____/|___/    \_| |_/\_| |_/\_| |_/|______|  \_/   /_____/|_____|\_| \_|
+       __   __  _____  ____      _____  __   _   _____  _     __    __  _____ _____ ______             
+      |  \/  |/  _  \|  _ \    /  _  \ |  \ | | /  _  \| |    \ \  / / |___  ||  ___|| ___ \            
+      | .  . || | | || | | |   | |_| | |   \| | | |_| || |     \ \/ /     / / | |__  | |_/ /            
+      | |\/| || | | || | | |   |  _  | | |\   | |  _  || |      \  /     / /  |  __| |    /             
+      | |  | || |_| || |/ /    | | | | | | \  | | | | || |____   | |    / /___| |___ | |\ \             
+      \_|  |_/\_____/|___/     \_| |_/ \_| |__/ \_| |_/|______|  \_/   /_____/|_____|\_| \_|
 "@
 
 Write-Host $asciiTitle -ForegroundColor Blue
 Write-Host ""
 
-# Create subtitle box
+# Create subtitle line style matching the image
 $subtitleText = "YarpLetapStan's Mod Analyzer V5.0"
-$boxWidth = $subtitleText.Length + 4
-$border = "=" * $boxWidth
-Write-Host "+$border+" -ForegroundColor Cyan
-Write-Host "|  $subtitleText  |" -ForegroundColor Cyan
-Write-Host "+$border+" -ForegroundColor Cyan
+$lineWidth = 80
+$topBorder = "=" * $lineWidth
+$bottomBorder = "=" * $lineWidth
+
+Write-Host $topBorder -ForegroundColor Blue
+Write-Host $subtitleText.PadLeft(($lineWidth + $subtitleText.Length) / 2) -ForegroundColor Cyan
+Write-Host $bottomBorder -ForegroundColor Blue
 Write-Host ""
