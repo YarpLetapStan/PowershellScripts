e89360a91a834b4f22ed6949230d7dd1ea2d4765
YarpLetapStan <salsa.isipog@gmail.com>
2026-05-06 20:33:10 -0700

    Update YarpsModAnalyzer6.0.ps1

--- YarpsModAnalyzer6.0.ps1
+++ YarpsModAnalyzer6.0.ps1
@@ -645,7 +645,11 @@ if ($cheatMods.Count -gt 0) {
             $sign = if ($mod.SizeDiffKB -gt 0){"+"} else {""}
             Write-Host "  ║ " -NoNewline -ForegroundColor Red
             if ($mod.SizeDiffKB -eq 0) { Write-Host "Size matches Modrinth: $($mod.ExpectedSizeKB) KB ✓" -ForegroundColor White }
-            else { Write-Host "Size: $($mod.FileSizeKB) KB (Expected: $($mod.ExpectedSizeKB) KB) | Difference: $sign$($mod.SizeDiffKB) KB" -ForegroundColor White }
+            else {
+    Write-Host "  ║ " -NoNewline -ForegroundColor Red
+    Write-Host "Size: $($mod.FileSizeKB) KB (Expected: $($mod.ExpectedSizeKB) KB) | " -NoNewline -ForegroundColor White
+    Write-Host "Difference: $sign$($mod.SizeDiffKB) KB" -ForegroundColor Red
+}
         }
         Write-Host "  ╚══════════════════════════════════════════`n" -ForegroundColor Red
     }
